704ecbc94e4aa380d858a382a6445cc55703b012
Jim McDonald <Jim@mcdee.net>
2021-11-17 20:37:13 +0000

    Merge fix.

--- services/metrics/prometheus/service.go
+++ services/metrics/prometheus/service.go
@@ -29,8 +29,8 @@ import (
 type Service struct {
 	chainTime chaintime.Service
 
-	schedulerJobsScheduled prometheus.Counter
-	schedulerJobsCancelled prometheus.Counter
+	schedulerJobsScheduled *prometheus.CounterVec
+	schedulerJobsCancelled *prometheus.CounterVec
 	schedulerJobsStarted   *prometheus.CounterVec
 
 	epochsProcessed   prometheus.Counter
