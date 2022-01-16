dd2b1055c8f3104694547b80b82ecfd05eb2d187
Jim McDonald <Jim@mcdee.net>
2022-01-16 18:45:52 +0000

    Update dependencies.

--- services/metrics/prometheus/synccommitteeaggregation.go
+++ services/metrics/prometheus/synccommitteeaggregation.go
@@ -1,4 +1,4 @@
-// Copyright © 2021 Attestant Limited.
+// Copyright © 2021, 2022 Attestant Limited.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -100,7 +100,7 @@ func (s *Service) SyncCommitteeAggregationsCompleted(started time.Time, slot pha
 		for i := 0; i < count; i++ {
 			s.syncCommitteeAggregationProcessTimer.Observe(duration)
 		}
-		secsSinceStartOfSlot := time.Since(s.chainTime.StartOfSlot(s.chainTime.CurrentSlot())).Seconds()
+		secsSinceStartOfSlot := time.Since(s.chainTime.StartOfSlot(slot)).Seconds()
 		s.syncCommitteeAggregationMarkTimer.Observe(secsSinceStartOfSlot)
 		s.syncCommitteeAggregationProcessLatestSlot.Set(float64(slot))
 	}
