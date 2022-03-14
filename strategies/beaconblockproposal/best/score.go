479c8b19de8f2b9b5014a2f198888f4099be6639
Jim McDonald <Jim@mcdee.net>
2022-03-14 09:53:12 +0000

    Score bellatrix blocks with best beaconblock proposal.

--- strategies/beaconblockproposal/best/score.go
+++ strategies/beaconblockproposal/best/score.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/attestantio/go-eth2-client/spec"
 	"github.com/attestantio/go-eth2-client/spec/altair"
+	"github.com/attestantio/go-eth2-client/spec/bellatrix"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
 	"github.com/prysmaticlabs/go-bitfield"
 )
@@ -44,6 +45,8 @@ func (s *Service) scoreBeaconBlockProposal(ctx context.Context,
 		return s.scorePhase0BeaconBlockProposal(ctx, name, parentSlot, blockProposal.Phase0)
 	case spec.DataVersionAltair:
 		return s.scoreAltairBeaconBlockProposal(ctx, name, parentSlot, blockProposal.Altair)
+	case spec.DataVersionBellatrix:
+		return s.scoreBellatrixBeaconBlockProposal(ctx, name, parentSlot, blockProposal.Bellatrix)
 	default:
 		log.Error().Int("version", int(blockProposal.Version)).Msg("Unhandled block version")
 		return 0
@@ -203,6 +206,97 @@ func (s *Service) scoreAltairBeaconBlockProposal(ctx context.Context,
 	return attestationScore + proposerSlashingScore + attesterSlashingScore + syncCommitteeScore
 }
 
+// scoreBellatrixBeaconBlockPropsal generates a score for a bellatrix beacon block.
+func (s *Service) scoreBellatrixBeaconBlockProposal(ctx context.Context,
+	name string,
+	parentSlot phase0.Slot,
+	blockProposal *bellatrix.BeaconBlock,
+) float64 {
+	attestationScore := float64(0)
+	immediateAttestationScore := float64(0)
+
+	// We need to avoid duplicates in attestations.
+	// Map is attestation slot -> committee index -> validator committee index -> aggregate.
+	attested := make(map[phase0.Slot]map[phase0.CommitteeIndex]bitfield.Bitlist)
+	for _, attestation := range blockProposal.Body.Attestations {
+		data := attestation.Data
+		if _, exists := attested[data.Slot]; !exists {
+			attested[data.Slot] = make(map[phase0.CommitteeIndex]bitfield.Bitlist)
+		}
+		if _, exists := attested[data.Slot][data.Index]; !exists {
+			if !exists {
+				attested[data.Slot][data.Index] = bitfield.NewBitlist(attestation.AggregationBits.Len())
+			}
+		}
+
+		priorVotes, err := s.priorVotesForAttestation(ctx, attestation, blockProposal.ParentRoot)
+		if err != nil {
+			log.Debug().Err(err).Msg("Failed to obtain prior votes for attestation; assuming no votes")
+		}
+
+		votes := 0
+		for i := uint64(0); i < attestation.AggregationBits.Len(); i++ {
+			if attestation.AggregationBits.BitAt(i) {
+				if attested[attestation.Data.Slot][attestation.Data.Index].BitAt(i) {
+					// Already attested in this block; skip.
+					continue
+				}
+				if priorVotes.BitAt(i) {
+					// Attested in a previous block; skip.
+					continue
+				}
+				votes++
+				attested[attestation.Data.Slot][attestation.Data.Index].SetBitAt(i, true)
+			}
+		}
+
+		// Now we know how many new votes are in this attestation we can score it.
+		// We can calculate if the head vote is correct, but not target so for the
+		// purposes of the calculation we assume that it is.
+
+		headCorrect := bellatrixHeadCorrect(blockProposal, attestation)
+		targetCorrect := s.bellatrixTargetCorrect(ctx, attestation)
+		inclusionDistance := blockProposal.Slot - attestation.Data.Slot
+
+		score := 0.0
+		if targetCorrect {
+			// Target is correct (and timely).
+			score += float64(s.timelyTargetWeight) / float64(s.weightDenominator)
+		}
+		if inclusionDistance <= 5 {
+			// Source is timely.
+			score += float64(s.timelySourceWeight) / float64(s.weightDenominator)
+		}
+		if headCorrect && inclusionDistance == 1 {
+			score += float64(s.timelyHeadWeight) / float64(s.weightDenominator)
+		}
+		score *= float64(votes)
+		attestationScore += score
+		if inclusionDistance == 1 {
+			immediateAttestationScore += score
+		}
+	}
+
+	attesterSlashingScore, proposerSlashingScore := scoreSlashings(blockProposal.Body.AttesterSlashings, blockProposal.Body.ProposerSlashings)
+
+	// Add sync committee score.
+	syncCommitteeScore := float64(blockProposal.Body.SyncAggregate.SyncCommitteeBits.Count()) * float64(s.syncRewardWeight) / float64(s.weightDenominator)
+
+	log.Trace().
+		Uint64("slot", uint64(blockProposal.Slot)).
+		Uint64("parent_slot", uint64(parentSlot)).
+		Str("provider", name).
+		Float64("immediate_attestations", immediateAttestationScore).
+		Float64("attestations", attestationScore).
+		Float64("proposer_slashings", proposerSlashingScore).
+		Float64("attester_slashings", attesterSlashingScore).
+		Float64("sync_committee", syncCommitteeScore).
+		Float64("total", attestationScore+proposerSlashingScore+attesterSlashingScore+syncCommitteeScore).
+		Msg("Scored Altair block")
+
+	return attestationScore + proposerSlashingScore + attesterSlashingScore + syncCommitteeScore
+}
+
 func scoreSlashings(attesterSlashings []*phase0.AttesterSlashing,
 	proposerSlashings []*phase0.ProposerSlashing,
 ) (float64, float64) {
@@ -281,6 +375,35 @@ func altairHeadCorrect(blockProposal *altair.BeaconBlock, attestation *phase0.At
 
 // altairTargetCorrect calculates if the target of an Altair attestation is correct.
 func (s *Service) altairTargetCorrect(_ context.Context,
+	attestation *phase0.Attestation,
+) bool {
+	s.priorBlocksMu.RLock()
+	defer s.priorBlocksMu.RUnlock()
+	root := attestation.Data.BeaconBlockRoot
+	maxSlot := s.chainTime.FirstSlotOfEpoch(attestation.Data.Target.Epoch)
+	for {
+		priorBlock, exists := s.priorBlocks[root]
+		if !exists {
+			// We don't have data on this block, assume the target is correct.
+			// (We could assume the target is incorrect in this situation, but that
+			// would give false incorrects whilst the prior block cache warms up.)
+			log.Trace().Uint64("attestation_slot", uint64(attestation.Data.Slot)).Uint64("max_slot", uint64(maxSlot)).Str("root", fmt.Sprintf("%#x", root)).Msg("Root does not exist, assuming true")
+			return true
+		}
+		if priorBlock.slot <= maxSlot {
+			return bytes.Equal(attestation.Data.Target.Root[:], priorBlock.root[:])
+		}
+		root = priorBlock.parent
+	}
+}
+
+// bellatrixHeadCorrect calculates if the head of a Bellatrix attestation is correct.
+func bellatrixHeadCorrect(blockProposal *bellatrix.BeaconBlock, attestation *phase0.Attestation) bool {
+	return bytes.Equal(blockProposal.ParentRoot[:], attestation.Data.BeaconBlockRoot[:])
+}
+
+// bellatrixTargetCorrect calculates if the target of a Bellatrix attestation is correct.
+func (s *Service) bellatrixTargetCorrect(_ context.Context,
 	attestation *phase0.Attestation,
 ) bool {
 	s.priorBlocksMu.RLock()
