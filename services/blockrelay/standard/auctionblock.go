f764353771ffe36b6a5bf94ec25ce0520bdd0609
Jim McDonald <Jim@mcdee.net>
2022-12-05 22:32:27 +0000

    Re-add missing variable.

--- services/blockrelay/standard/auctionblock.go
+++ services/blockrelay/standard/auctionblock.go
@@ -56,6 +56,7 @@ func (s *Service) AuctionBlock(ctx context.Context,
 ) {
 	ctx, span := otel.Tracer("attestantio.vouch.services.blockrelay.standard").Start(ctx, "AuctionBlock")
 	defer span.End()
+	started := time.Now()
 
 	s.executionConfigMu.RLock()
 	proposerConfig, exists := s.executionConfig.ProposerConfigs[pubkey]
