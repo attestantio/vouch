df284c300f186dfa03b7cc996ab4b726c03f3b7f
Jim McDonald <Jim@mcdee.net>
2024-01-16 23:33:23 +0000

    Update release version.

--- main.go
+++ main.go
@@ -108,7 +108,7 @@ import (
 )
 
 // ReleaseVersion is the release version for the code.
-var ReleaseVersion = "1.8.0-beta.1"
+var ReleaseVersion = "1.8.0-beta.3"
 
 func main() {
 	exitCode := main2()
